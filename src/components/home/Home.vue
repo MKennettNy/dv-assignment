ac302ee03b7379a7684b18e8d0049ad523196486
Ashleigh Mclaughlin <932400796@dov2-sc-23.local>
2019-03-25 18:11:37 +1300

    First commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Hello</h1>
+      <MyFooter />
+  </div>
+</template>
+
+<script>
+import MyFooter from "./MyFooter";
+
+export default {
+  name: "Home",
+  components: {
+      MyFooter
+  }
+};
+</script>
+
+<style scoped></style>
